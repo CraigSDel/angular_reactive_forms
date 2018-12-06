87825578b277439405de563eb4a9d36296eb56d4
Craig Stroberg <craig@psybergate.co.za>
2018-12-06 10:54:10 +0200

    Reactive Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
@@ -6,18 +6,18 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   rForm: FormGroup;
-  post: any;
   description: string = '';
   name: string = '';
+  titleAlert: string = 'This field is required';
 
   constructor(private fb: FormBuilder) {
     this.rForm = fb.group({
         'name': [null, Validators.required],
         'description': [null, Validators.compose([Validators.required,
-                                                          Validators.minLength(3),
-                                                          Validators.maxLength(10)])],
+          Validators.minLength(30),
+          Validators.maxLength(500)])],
         'validate': ''
       }
     );
@@ -26,5 +26,19 @@ export class AppComponent {
   addPost(post) {
     this.description = post.description;
     this.name = post.name;
+  }
+
+  ngOnInit(): void {
+    this.rForm.get('validate').valueChanges.subscribe(
+      (validate) => {
+        if (validate == '1') {
+          this.rForm.get('name').setValidators([Validators.required, Validators.minLength(3)]);
+          this.titleAlert = "You need to specify at least 3 characters";
+        } else {
+          this.rForm.get('name').setValidators(Validators.required);
+        }
+        this.rForm.get('name').updateValueAndValidity();
+      }
+    );
   }
 }
